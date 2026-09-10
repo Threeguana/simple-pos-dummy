f1790555f8cd40d215a3028a2b4b2c422d8e3aae
Boby Rozak Saputra <2341760162@student.polinema.ac.id>
2026-09-10 16:32:39 +0700

    tambah @section

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -55,7 +55,7 @@ class TransactionController extends Controller
 
     public function index()
     {
-        return 'Daftar transaksi';
+        return view ('trasactions.index');
     }
 
     public function show(string $id)
